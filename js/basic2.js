634eed01455049b4fcb0adb82d72fb0490fc6f2f
Airubby <ai_rubby@sina.cn>
2016-11-07 17:44:23 +0800

    动态增加版块二的span的class让人随机显示

--- js/basic2.js
+++ js/basic2.js
@@ -1,3 +1,4 @@
+//提示和点击加入vip的效果
 var black=document.getElementById("black");
 var popTwo=document.getElementById('popTwo');
 var closeTwo=document.getElementById("closeTwo");
@@ -6,25 +7,40 @@ var closeOne=document.getElementById("closeOne");
 var ship=document.getElementById("ship");
 var shipA=ship.getElementsByTagName('a')[0];
 var shipStar=document.getElementById("shipStar");
+//动态给页面span增加class
+var arr=[];
+var json={};
+var manName = [ 'man', 'oldman', 'boy' ];
+var womanName = [ 'woman', 'girl' ];
+var manNum=0;
+var womanNum=0;
+var onePieceSpan=document.getElementById('pieceOne').getElementsByTagName('span');
+var twoPieceSpan = document.getElementById('pieceTwo').getElementsByTagName('span');
+var threePieceSpan = document.getElementById('pieceThree').getElementsByTagName('span');
+var spanNum = onePieceSpan.length+twoPieceSpan.length/2+threePieceSpan.length;
 
+
+
+//提示和点击加入vip的效果
 if(!sessionStorage.getItem('username')){
 	setTimeout(function(){
 		black.style.display="block";
 		//popTwo.style["WebkitTransform"]=popTwo.style["transform"]="translateY("+black.offsetHeight/2+"px)";
 		css(popTwo,"translateY",black.offsetHeight/2);
-		closeTwo.timer=setInterval(function(){
-			css(popTwo,"translateY",-black.offsetHeight/2);
-			setInterval(function(){
-				black.style.display="none";
-			},500);
-		},8000);
 		closeTwo.onclick=function(){
 			clearInterval(this.timer);
 			css(popTwo,"translateY",-black.offsetHeight/2);
-			setInterval(function(){
+			setTimeout(function(){
 				black.style.display="none";
 			},500);
 		}
+		closeTwo.timer=setTimeout(function(){
+			css(popTwo,"translateY",-black.offsetHeight/2);
+			setTimeout(function(){
+				black.style.display="none";
+			},500);
+		},8000);
+		
 	},1000);
 }
 shipA.onclick=function(){
@@ -56,18 +72,48 @@ closeOne.onclick=function(){
 			move(ship, {translateX:0,translateY:0}, 1000, 'backOut',function(){
 				css(shipStar,"opacity",1);
 				shipA.style.WebkitTransform=shipA.style.transform='scale(1,1)';
-					/*
-					oJoinVip.style.cssText = '';
-					oStar.style.opacity = 1;*/
-					
 			});
 		},500);
 	},500);
 }
 
-
-
-
+//动态给页面span增加class
+while(arr.length<spanNum){
+	var num=Math.floor(Math.random()*data.length);
+	if(!json[num]){
+		arr.push(num);
+		json[num]=1;
+	}
+}
+for(var i=0;i<onePieceSpan.length;i++){
+	if(data[arr[i]].sex=="man"){
+		onePieceSpan[i].className=manName[manNum%manName.length];
+		manNum++;
+	}else{
+		onePieceSpan[i].className=womanName[womanNum%womanName.length];
+		womanNum++;
+	}
+	
+}
+for(var i=0;i<twoPieceSpan.length/2;i++){
+	if(data[arr[i+onePieceSpan.length]].sex=="man"){
+		twoPieceSpan[i].className=twoPieceSpan[i+twoPieceSpan.length/2].className=manName[manNum%manName.length];
+		manNum++;
+	}else{
+		twoPieceSpan[i].className=twoPieceSpan[i+twoPieceSpan.length/2].className=womanName[womanNum%womanName.length];
+		womanNum++;
+	}
+	
+}
+for(var i=0;i<threePieceSpan.length;i++){
+	if(data[arr[i+onePieceSpan.length+twoPieceSpan.length/2]].sex=="man"){
+		threePieceSpan[i].className=manName[manNum%manName.length];
+		manNum++;
+	}else{
+		threePieceSpan[i].className=womanName[womanNum%womanName.length];
+		womanNum++;
+	}
+}
 
 
 
